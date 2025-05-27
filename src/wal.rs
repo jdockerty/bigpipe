7fcefd701e36af46a44698a85f74b88084a5c40e
Jack <56563911+jdockerty@users.noreply.github.com>
2025-05-27 21:01:48 +0100

    feat: persistence via wal (#1)
    * chore: deps
    
    * feat: add ServerMessage try_into for Vec<u8>
    
    * feat: wip wal
    
    * feat: add rotation semantics
    
    * feat: add rotation to wal write path
    
    * feat: add rotation tests
    
    * feat: wire up wal to server

---
+++ src/wal.rs
@@ -0,0 +1,197 @@
+use std::convert::TryInto;
+use std::path::Path;
+use std::{fs::File, io::Write, path::PathBuf};
+
+use crate::ServerMessage;
+
+pub(crate) const DEFAULT_MAX_SEGMENT_SIZE: usize = 16777216; // 16 MiB
+const MAX_SEGMENT_BUFFER_SIZE: u16 = 8192; // 8 KiB
+
+const WAL_EXTENSION: &str = "-bp.wal";
+const WAL_DEFAULT_ID: u64 = 0;
+
+#[derive(Debug)]
+pub struct Wal {
+    id: u64,
+    active_segment: Segment,
+    directory: PathBuf,
+}
+
+impl Wal {
+    pub fn new(directory: PathBuf, segment_max_size: Option<usize>) -> Self {
+        // TODO: detect pre-existing segments, for now it always
+        // starts at 0.
+        Self {
+            id: WAL_DEFAULT_ID,
+            directory: directory.clone(),
+            active_segment: Segment::new(
+                directory.join(format!("{WAL_DEFAULT_ID}{WAL_EXTENSION}")),
+                segment_max_size,
+            ),
+        }
+    }
+
+    /// Write a message into the write-ahead log.
+    pub fn write(&mut self, message: &ServerMessage) -> Result<usize, Box<dyn std::error::Error>> {
+        if self.active_segment.current_size >= self.active_segment.file_max_size {
+            self.rotate()?;
+        }
+        self.active_segment.write(message)
+    }
+
+    /// Force flush the current active segment.
+    pub(crate) fn flush(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        self.active_segment.flush()
+    }
+
+    /// Rotate the currently active WAL segment.
+    pub(crate) fn rotate(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        // Force a flush to ensure that no data is lost before rotation.
+        self.active_segment.flush()?;
+
+        self.id += 1;
+
+        self.active_segment = Segment::new(
+            self.directory.join(format!("{}{WAL_EXTENSION}", self.id)),
+            Some(self.active_segment.file_max_size),
+        );
+        Ok(())
+    }
+
+    pub fn active_segment_path(&self) -> &Path {
+        self.active_segment.path()
+    }
+}
+
+#[derive(Debug)]
+struct Segment {
+    filepath: PathBuf,
+    file: File,
+    current_size: usize,
+    file_max_size: usize,
+    buf: Vec<u8>,
+}
+
+impl Segment {
+    fn new(segment_path: PathBuf, segment_max_size: Option<usize>) -> Self {
+        let buf = Vec::with_capacity(MAX_SEGMENT_BUFFER_SIZE as usize);
+        Self {
+            filepath: segment_path.clone(),
+            file: File::create_new(segment_path).unwrap(),
+            file_max_size: segment_max_size.unwrap_or(DEFAULT_MAX_SEGMENT_SIZE),
+            current_size: 0,
+            buf,
+        }
+    }
+
+    fn write(&mut self, message: &ServerMessage) -> Result<usize, Box<dyn std::error::Error>> {
+        let message_bytes: Vec<u8> = message.try_into()?;
+        let size = message_bytes.len();
+
+        self.buf.write_all(&message_bytes)?;
+
+        if self.buf.len() >= MAX_SEGMENT_BUFFER_SIZE as usize {
+            self.flush()?;
+        }
+
+        Ok(size)
+    }
+
+    fn flush(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        self.file.write_all(&self.buf)?;
+        self.file.sync_all()?;
+        self.current_size += self.buf.len();
+        self.buf.clear();
+        Ok(())
+    }
+
+    /// Get the path to the currently active segment file.
+    fn path(&self) -> &Path {
+        &self.filepath
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use tempfile::TempDir;
+
+    use super::*;
+
+    #[test]
+    fn path_semantics() {
+        let dir = TempDir::new().unwrap();
+
+        let mut wal = Wal::new(dir.path().to_path_buf(), None);
+
+        let expected_path = dir.path().join(format!("{WAL_DEFAULT_ID}{WAL_EXTENSION}"));
+        assert!(expected_path.exists());
+        assert_eq!(wal.active_segment.path(), &expected_path);
+        assert_eq!(wal.id, 0);
+
+        wal.rotate().unwrap();
+        let expected_path = dir.path().join(format!("1{WAL_EXTENSION}"));
+        assert!(expected_path.exists());
+        assert_eq!(wal.active_segment.path(), &expected_path);
+        assert_eq!(wal.id, 1);
+    }
+
+    #[test]
+    fn write() {
+        let dir = TempDir::new().unwrap();
+
+        let mut wal = Wal::new(dir.path().to_path_buf(), None);
+
+        wal.write(&ServerMessage {
+            key: "hello".to_string(),
+            value: "world".into(),
+            timestamp: 1,
+        })
+        .unwrap();
+
+        wal.flush().unwrap();
+
+        assert!(wal.active_segment_path().exists());
+        assert_ne!(
+            wal.active_segment.file.metadata().unwrap().len(),
+            0,
+            "Flushing WAL expected a non-zero file size after writing"
+        );
+    }
+
+    #[test]
+    fn write_with_rotation() {
+        let dir = TempDir::new().unwrap();
+
+        let mut wal = Wal::new(dir.path().to_path_buf(), Some(64));
+
+        // Known size of the write
+        let server_msg_size = 34;
+
+        for _ in 0..=2 {
+            wal.write(&ServerMessage {
+                key: "hello".to_string(),
+                value: "world".into(),
+                timestamp: 1,
+            })
+            .unwrap();
+            wal.flush().unwrap();
+        }
+
+        assert_eq!(wal.id, 1, "Expected rotation");
+
+        assert_eq!(
+            wal.active_segment.file.metadata().unwrap().len(),
+            server_msg_size,
+            "A single write should be in the new file"
+        );
+        assert_eq!(
+            std::fs::File::open(dir.path().join(format!("{WAL_DEFAULT_ID}{WAL_EXTENSION}")))
+                .unwrap()
+                .metadata()
+                .unwrap()
+                .len(),
+            server_msg_size * 2,
+            "Two writes are expected to the old segment"
+        )
+    }
+}

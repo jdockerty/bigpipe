3e704b4bdd7a9802060a300e68b8500897d7780b
Jack <56563911+jdockerty@users.noreply.github.com>
2025-05-27 21:10:11 +0100

    chore: misc cleanups (#2)
    * chore: remove debug
    
    * chore: lints and docs

--- src/bin/bigpipe.rs
+++ src/bin/bigpipe.rs
@@ -2,14 +2,11 @@ use std::{
     io::Write,
     net::{TcpListener, TcpStream},
     path::PathBuf,
-    sync::Arc,
-    time::Duration,
 };
 
 use clap::{Parser, Subcommand};
 
 use bigpipe::{BigPipe, ClientMessage, ServerMessage};
-use parking_lot::Mutex;
 
 #[derive(Parser)]
 struct Cli {
@@ -40,9 +37,6 @@ enum Commands {
         /// Max size of a segment for the WAL.
         #[arg(long, env = "BIGPIPE_WAL_SEGMENT_MAX_SIZE")]
         wal_segment_max_size: Option<usize>,
-
-        #[arg(long, hide = true)]
-        debug: bool,
     },
 }
 
@@ -61,34 +55,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
         Commands::Server {
             addr,
-            debug,
             wal_directory,
             wal_segment_max_size,
         } => {
-            let bigpipe = Arc::new(Mutex::new(BigPipe::new(
-                wal_directory,
-                wal_segment_max_size,
-            )));
+            let mut bigpipe = BigPipe::new(wal_directory, wal_segment_max_size);
 
             let listener = TcpListener::bind(addr).unwrap();
             println!("bigpipe running at {}", listener.local_addr().unwrap());
 
-            if debug {
-                let bigpipe_background = Arc::clone(&bigpipe);
-                std::thread::spawn(move || loop {
-                    std::thread::sleep(Duration::from_millis(500));
-                    println!("{:?}", bigpipe_background.lock().messages());
-                });
-            }
             for stream in listener.incoming() {
                 let stream = stream.unwrap();
                 let message: ClientMessage = rmp_serde::from_read(stream).unwrap();
 
                 let timestamp = chrono::Utc::now().timestamp_micros();
                 let server_msg: ServerMessage = message.into_server_message(timestamp);
-                let mut bigpipe_guard = bigpipe.lock();
-                bigpipe_guard.wal_write(&server_msg)?;
-                bigpipe_guard.add_message(server_msg);
+                bigpipe.wal_write(&server_msg)?;
+                bigpipe.add_message(server_msg);
             }
         }
     }

9b120e5af0ac953c79ab0e61af30296ee97c3e04
Jack Dockerty <jdockerty19@gmail.com>
2025-06-07 13:38:57 +0100

    refactor: separate proto for namespace

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .compile_protos(&["proto/wal.proto", "proto/message.proto"], &["proto"])?;
+    tonic_build::configure().compile_protos(
+        &[
+            "proto/wal.proto",
+            "proto/message.proto",
+            "proto/namespace.proto",
+        ],
+        &["proto"],
+    )?;
     Ok(())
 }

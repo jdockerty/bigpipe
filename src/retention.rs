e917533ca057f63a13294089e76d7094e95e8e94
Jack Dockerty <jdockerty19@gmail.com>
2025-08-06 22:10:43 +0100

    feat: expand to retention manager with various handles

--- src/retention.rs
+++ src/retention.rs
@@ -1,7 +1,70 @@
-use tokio::sync::mpsc::{channel, Receiver, Sender};
+use std::{sync::Arc, time::Duration};
 
+use hashbrown::HashMap;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
+use tracing::info;
+
+use crate::data_types::{BigPipeValue, Namespace};
+
+#[derive(Debug)]
+pub struct RetentionManager {
+    namespaces: HashMap<Namespace, RetentionHandle>,
+}
+
+impl RetentionManager {
+    pub fn new() -> Self {
+        Self {
+            namespaces: HashMap::new(),
+        }
+    }
+
+    pub async fn check_policy(&mut self, message: RetentionMessage) {
+        match message {
+            RetentionMessage::Ttl {
+                namespace,
+                max_duration,
+                values,
+            } => {
+                let handle = self
+                    .namespaces
+                    .entry(namespace.clone())
+                    .or_insert(RetentionHandle::new());
+                handle
+                    .send_message(RetentionMessage::Ttl {
+                        namespace,
+                        max_duration,
+                        values,
+                    })
+                    .await;
+            }
+            RetentionMessage::DiskPressure { namespace } => {
+                let handle = self
+                    .namespaces
+                    .entry(namespace.clone())
+                    .or_insert(RetentionHandle::new());
+                handle
+                    .send_message(RetentionMessage::DiskPressure { namespace })
+                    .await;
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum RetentionMessage {
+    Ttl {
+        namespace: Namespace,
+        max_duration: Duration,
+        values: Arc<BigPipeValue>,
+    },
+    DiskPressure {
+        namespace: Namespace,
+    },
+}
+
+#[derive(Debug)]
 pub struct RetentionHandle {
-    tx: Sender<()>,
+    tx: Sender<RetentionMessage>,
 }
 
 impl RetentionHandle {
@@ -10,20 +73,24 @@ impl RetentionHandle {
         tokio::spawn(start_retention(RetentionActor::new(rx)));
         Self { tx }
     }
+
+    async fn send_message(&self, message: RetentionMessage) {
+        self.tx.send(message).await.unwrap()
+    }
 }
 
 struct RetentionActor {
-    rx: Receiver<()>,
+    rx: Receiver<RetentionMessage>,
 }
 
 impl RetentionActor {
-    fn new(rx: Receiver<()>) -> Self {
+    fn new(rx: Receiver<RetentionMessage>) -> Self {
         Self { rx }
     }
 }
 
 async fn start_retention(mut actor: RetentionActor) {
-    while let Some(_) = actor.rx.recv().await {
-        todo!()
+    while let Some(msg) = actor.rx.recv().await {
+        info!(?msg);
     }
 }

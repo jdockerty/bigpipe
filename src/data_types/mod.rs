4729a674e24214f29204b3ffad062fc7dd62f81b
Jack Dockerty <jdockerty19@gmail.com>
2025-08-28 21:53:14 +0100

    refactor: log proto exposure

--- src/data_types/mod.rs
+++ src/data_types/mod.rs
@@ -3,6 +3,6 @@ pub mod message;
 pub mod namespace;
 pub mod value;
 
-pub use log::log as log_proto;
+pub use log as log_proto;
 pub use message::message as message_proto;
 pub use namespace::namespace as namespace_proto;
